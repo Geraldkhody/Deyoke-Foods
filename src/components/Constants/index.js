761590254a92a7bad0fb9ab94aa4b7c26f19e0eb
Stanislaus <geraldkhody@gmail.com>
2024-10-22 17:01:08 +0000

    Init commit

---
+++ src/components/Constants/index.js
@@ -0,0 +1,9 @@
+import logo from "../../assets/logo.png";
+import home_bg from "../../assets/home_background.png";
+
+
+export default {
+    logo,
+    home_bg,
+ 
+}
---
+++ src/components/Home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="background">
+      <img :src="home_bg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+import home_bg from "../Constants"
+
+export default {
+    name: "Home",
+};
+</script>
+
+<style scoped></style>
